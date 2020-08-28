0cf2e20a01a4f255f9ef8b7ba228def933d5ced8
ucLh <luchwork@mail.ru>
2020-08-28 10:09:54 +0300

    Refactored inference into class.

--- src/pytorch_infer.py
+++ src/pytorch_infer.py
@@ -44,7 +44,6 @@ class Model:
 
 class ModelNames(enum.Enum):
     eff_net_v2 = 'effnetb7_mixup_v2'
-    eff_net = 'effnetb7_mixup'
     se_resnext50 = 'unet_se_resnext50'
     resnet34 = 'unet_resnet34'
     mobilenet2 = 'unet_mobilenet2'
@@ -63,39 +62,128 @@ def create_transforms(additional):
     return res
 
 
-def calculate_dice_and_iou(model, datasets, loaders, mask_df, thresholds, min_area):
-    def tta_mean(preds, labels=None):
+class ModelInferenceHandler:
+    def __init__(self, mask_df):
+        self.mask_df = mask_df
+        self.thresholds = [0.5, 0.5, 0.5, 0.5]
+        self.min_area = [600, 600, 1000, 2000]
+        self.res = []
+        self.dice_score_list = []
+        self.dice_score_list_ch = []
+        self.iou_score_list = []
+
+    @staticmethod
+    def _tta_mean(preds):
         preds = torch.stack(preds)
         preds = torch.mean(preds, dim=0)
-        if labels is not None:
-            labels = torch.stack(labels)
-            labels = torch.mean(labels, dim=0)
-            labels = labels.detach().cpu().numpy()  # has shape (1, 4)
-            labels = labels[0]
+        return preds.detach().cpu().numpy()
 
-        return preds.detach().cpu().numpy(), labels
+    def _make_predicition(self, labels, file, p, p_img):
+        for i in range(4):
+            p_channel = np.zeros((256, 1600), dtype=np.uint8)
+            imageid_classid = file + '_' + str(i + 1)
+            if (labels is None) or (labels[i] > 0):
+                p_channel = p[i]
+                p_channel = (p_channel > self.thresholds[i]).astype(np.uint8)
+                if p_channel.sum() < self.min_area[i]:
+                    p_channel = np.zeros(p_channel.shape, dtype=p_channel.dtype)
+            # Save channel to obtain all channels for metrics calculation
+            p_img.append(p_channel)
+            self.res.append({
+                'ImageId_ClassId': imageid_classid,
+                'EncodedPixels': mask2rle(p_channel)
+            })
 
-    def batch_postprocessing():
+    def _batch_processing(self, image_file, j, labels, preds):
         for p, file in zip(preds, image_file):
             file = os.path.basename(file)
             # Image postprocessing
             p_img = []
-            _, target = make_mask_custom(j, mask_df)
+            _, target = make_mask_custom(j, self.mask_df)
             target = torch.tensor(target[np.newaxis, :])
-            for i in range(4):
-                p_channel = np.zeros((256, 1600), dtype=np.uint8)
-                imageid_classid = file + '_' + str(i + 1)
-                if (labels is None) or (labels[i] > 0):
-                    p_channel = p[i]
+            self._make_predicition(labels, file, p, p_img)
+
+            p_img = np.array(p_img)
+            # Make it seem like a batch of 1
+            p_img = torch.tensor(p_img[np.newaxis, :])
+
+            self.dice_score_list_ch.append(dice_single_channel(p_img[0, 2], target[0, 2], 0.5).numpy())
+            self.dice_score_list.append(dice_channel_torch(p_img, target, 0.5).numpy())
+            self.iou_score_list.append(compute_iou_batch(p_img, target, classes=[1]))
+
+    def _infer_net(self, datasets, loaders, model, total):
+        with torch.no_grad():
+            for j, loaders_batch in enumerate(tqdm(zip(*loaders), total=total)):
+                preds, image_file = [], []
+                labels = None
+                for i, batch in enumerate(loaders_batch):
+                    features = batch['features'].cuda()
+                    output = model(features)
+                    if type(output) is tuple:
+                        output, labels = output
+                    p = torch.sigmoid(output)
+                    # inverse operations for TTA
+                    p = datasets[i].inverse(p)
+                    preds.append(p)
+                    image_file = batch['image_file']
+
+                # TTA mean
+                preds = self._tta_mean(preds)
+                if labels is not None   :
+                    labels = self._tta_mean(labels)
+                    labels = labels[0]
+
+                # Batch post processing
+                self._batch_processing(image_file, j, labels, preds)
+
+    def calculate_dice_and_iou(self, model, datasets, loaders):
+        self.res, self.dice_score_list, self.dice_score_list_ch, self.iou_score_list = [], [], [], []
+        total = len(datasets[0]) // 1  # Todo: Add batch size
+        self._infer_net(datasets, loaders, model, total)
+        dice = np.mean(self.dice_score_list)
+        dice_ch = np.mean(self.dice_score_list_ch)
+        iou = np.nanmean(self.iou_score_list)
+        return dice, dice_ch, iou, self.res
+
+
+def calculate_dice_and_iou_ensemble(model, cls, datasets, loaders, mask_df, thresholds, min_area):
+    def write_prediction(res, imageid_classid, p_channel):
+        res.append({'ImageId_ClassId': imageid_classid,
+                    'EncodedPixels': mask2rle(p_channel)})
+
+    def make_zeroed_prediction(res, file):
+        for i in range(4):
+            imageid_classid = file + '_' + str(i + 1)
+            p_channel = np.zeros((256, 1600), dtype=np.uint8)
+            write_prediction(res, imageid_classid, p_channel)
+
+    def make_predicition(labels, file, p, p_aux, p_img):
+        for i in range(4):
+            p_channel = np.zeros((256, 1600), dtype=np.uint8)
+            imageid_classid = file + '_' + str(i + 1)
+            if labels[i] > 0:
+                p_channel = p[i]
+                p_channel = (p_channel > thresholds[i]).astype(np.uint8)
+                if p_channel.sum() < min_area[i]:
+                    p_channel = np.zeros(p_channel.shape, dtype=p_channel.dtype)
+                else:
+                    p_channel = (p[i] + p_aux[i]) / 2  # Take mean
                     p_channel = (p_channel > thresholds[i]).astype(np.uint8)
                     if p_channel.sum() < min_area[i]:
                         p_channel = np.zeros(p_channel.shape, dtype=p_channel.dtype)
-                # Save channel to obtain all channels for metrics calculation
-                p_img.append(p_channel)
-                res.append({
-                    'ImageId_ClassId': imageid_classid,
-                    'EncodedPixels': mask2rle(p_channel)
-                })
+            p_img.append(p_channel)
+            res.append({
+                'ImageId_ClassId': imageid_classid,
+                'EncodedPixels': mask2rle(p_channel)
+            })
+
+    def batch_processing(image_file, j, labels, preds, preds_aux):
+        for p, p_aux, file in zip(preds, preds_aux, image_file):
+            file = os.path.basename(file)
+            p_img = []
+            _, target = make_mask_custom(j, mask_df)
+            target = torch.tensor(target[np.newaxis, :])
+            make_predicition(labels, file, p, p_aux, p_img)
 
             p_img = np.array(p_img)
             # Make it seem like a batch of 1
@@ -105,28 +193,38 @@ def calculate_dice_and_iou(model, datasets, loaders, mask_df, thresholds, min_ar
             dice_score_list.append(dice_channel_torch(p_img, target, 0.5).numpy())
             iou_score_list.append(compute_iou_batch(p_img, target, classes=[1]))
 
+    def infer_net(datasets, loaders, model, total):
+        with torch.no_grad():
+            for j, loaders_batch in enumerate(tqdm(zip(*loaders), total=total)):
+                preds, preds_aux, labels = [], [], []
+                image_file = []
+                for i, batch in enumerate(loaders_batch):
+                    features = batch['features'].cuda()
+                    pred_aux, label = cls(features)
+                    pred_raw, _ = model(features)
+                    labels.append(label)
+                    p = torch.sigmoid(pred_raw)
+                    p_aux = torch.sigmoid(pred_aux)
+                    image_file = batch['image_file']
+
+                    # inverse operations for TTA
+                    p = datasets[i].inverse(p)
+                    p_aux = datasets[i].inverse(p_aux)
+                    preds.append(p)
+                    preds_aux.append(p_aux)
+
+                # TTA mean
+                preds = ModelInferenceHandler._tta_mean(preds)
+                preds_aux = ModelInferenceHandler._tta_mean(preds_aux)
+                labels = ModelInferenceHandler._tta_mean(labels)
+                labels = labels[0]
+
+                # Batch post processing
+                batch_processing(image_file, j, labels, preds, preds_aux)
+
     res, dice_score_list, dice_score_list_ch, iou_score_list = [], [], [], []
     total = len(datasets[0]) // 1  # Todo: Add batch size
-    with torch.no_grad():
-        for j, loaders_batch in enumerate(tqdm(zip(*loaders), total=total)):
-            preds = []
-            image_file = []
-            for i, batch in enumerate(loaders_batch):
-                features = batch['features'].cuda()
-                output = model(features)
-                if type(output) is tuple:
-                    output, label = output
-                p = torch.sigmoid(output)
-                # inverse operations for TTA
-                p = datasets[i].inverse(p)
-                preds.append(p)
-                image_file = batch['image_file']
-
-            # TTA mean
-            preds, labels = tta_mean(preds)
-
-            # Batch post processing
-            batch_postprocessing()
+    infer_net(datasets, loaders, model, total)
 
     dice = np.mean(dice_score_list)
     dice_ch = np.mean(dice_score_list_ch)
@@ -147,9 +245,10 @@ def main(args):
     unet_mobilenet2 = load('../data/severstalmodels/unet_mobilenet2.pth').cuda()
     unet_resnet34 = load('../data/severstalmodels/unet_resnet34.pth').cuda()
     eff_net_v2 = load('../ckpt/traced_effnetb7_1024_mixup_v2.pth').cuda()
-    eff_net = load('../ckpt/effnetb0_final_stage/traced_effnetb0_averaged.pth').cuda()
+    eff_net = load('../ckpt/traced_effnetb7_mixup_retrain.pth').cuda()
+    cls = load('../ckpt/traced_effnetb0_classifier_maskdrop.pth').cuda()
 
-    models_list = [eff_net_v2, eff_net, unet_se_resnext50_32x4d, unet_resnet34, unet_mobilenet2]
+    models_list = [eff_net_v2, unet_se_resnext50_32x4d, unet_resnet34, unet_mobilenet2]
     models_dict = {}
     for i, model_name in enumerate(ModelNames):
         models_list[i].eval()
@@ -158,7 +257,7 @@ def main(args):
     # Different transforms for TTA wrapper
     transforms = [
         [],
-        [A.HorizontalFlip(p=1)],
+        # [A.HorizontalFlip(p=1)],
         # [A.VerticalFlip(p=1)],
     ]
 
@@ -170,10 +269,12 @@ def main(args):
     table.field_names = ['Model', 'dice', 'iou']
 
     logger = MetricsLogger.config_logger('metric_scores', '../logs/scores.log')
+    inference_handler = ModelInferenceHandler(mask_df)
 
     dice_dict, iou_dict = {}, {}
     for key in models_dict.keys():
-        dice, dice_ch, iou, res = calculate_dice_and_iou(models_dict[key], datasets, loaders, mask_df, thresholds, min_area)
+        dice, dice_ch, iou, res = \
+            inference_handler.calculate_dice_and_iou(models_dict[key], datasets, loaders)
         print(dice, iou)
         table.add_row([key, dice, iou])
         dice_dict[key] = dice
@@ -190,6 +291,9 @@ def main(args):
     df['empty'] = df['EncodedPixels'].map(lambda x: not x)
     classes = df[df['empty'] == False]['Class'].value_counts()
     print(classes)
+
+    dice, dice_ch, iou, res = calculate_dice_and_iou_ensemble(eff_net, cls, datasets, loaders, mask_df, thresholds, min_area)
+    print(dice, dice_ch, iou)
 
 
 def parse_arguments(argv):
